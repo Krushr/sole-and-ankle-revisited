81ffbba295ea6367c6e487437f7e6e36cb76c300
Adam Hardiman <715209+Krushr@users.noreply.github.com>
2022-02-01 20:33:14 +1000

    Define Breakpoints

--- src/constants.js
+++ src/constants.js
@@ -16,3 +16,15 @@ export const WEIGHTS = {
   medium: 600,
   bold: 800,
 };
+
+export const BREAKPOINTS = {
+  phoneMin: 600 / 16, // 600px / 16px = 37.5rem
+  tabletMin: 950 / 16, // 950px / 16px = 59.375rem
+  laptopMin: 1300 / 16, // 1300px / 16px = 81.25rem
+};
+
+export const QUERIES = {
+  phoneAndUp: `(min-width: ${BREAKPOINTS.phoneMin}rem)`,
+  tabletAndUp: `(min-width: ${BREAKPOINTS.tabletMin}rem)`,
+  laptopAndUp: `(min-width: ${BREAKPOINTS.laptopMin}rem)`,
+};
