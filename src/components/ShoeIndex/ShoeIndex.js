735c49e0840e1f29713030f066d1982350ac29e8
Adam Hardiman <715209+Krushr@users.noreply.github.com>
2022-02-01 22:07:08 +1000

    Tweak main view header for mobile & tablet

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,40 +1,47 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { WEIGHTS } from '../../constants';
+import { QUERIES, WEIGHTS } from "../../constants";
 
-import Breadcrumbs from '../Breadcrumbs';
-import Select from '../Select';
-import Spacer from '../Spacer';
-import ShoeSidebar from '../ShoeSidebar';
-import ShoeGrid from '../ShoeGrid';
+import Breadcrumbs from "../Breadcrumbs";
+import Select from "../Select";
+import Spacer from "../Spacer";
+import ShoeSidebar from "../ShoeSidebar";
+import ShoeGrid from "../ShoeGrid";
 
 const ShoeIndex = ({ sortId, setSortId }) => {
+  const breadCrumbs = (
+    <Breadcrumbs>
+      <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+      <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+      <Breadcrumbs.Crumb href="/sale/shoes">Shoes</Breadcrumbs.Crumb>
+    </Breadcrumbs>
+  );
+
   return (
     <Wrapper>
       <MainColumn>
         <Header>
-          <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
+          <Title>
+            <MobileBreadCrumbs>{breadCrumbs}</MobileBreadCrumbs>
+            Running
+          </Title>
+          <SortWrapper>
+            <Select
+              label="Sort"
+              value={sortId}
+              onChange={(ev) => setSortId(ev.target.value)}
+            >
+              <option value="newest">Newest Releases</option>
+              <option value="price">Price</option>
+            </Select>
+          </SortWrapper>
         </Header>
         <Spacer size={32} />
         <ShoeGrid />
       </MainColumn>
       <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
-        </Breadcrumbs>
+        {breadCrumbs}
         <Spacer size={42} />
         <ShoeSidebar />
       </LeftColumn>
@@ -50,7 +57,12 @@ const Wrapper = styled.div`
 `;
 
 const LeftColumn = styled.div`
+  display: none;
   flex-basis: 248px;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: revert;
+  }
 `;
 
 const MainColumn = styled.div`
@@ -58,14 +70,33 @@ const MainColumn = styled.div`
 `;
 
 const Header = styled.header`
+  position: relative;
   display: flex;
   justify-content: space-between;
-  align-items: baseline;
+  align-items: center;
+
+  @media ${QUERIES.laptopAndUp} {
+    align-items: baseline;
+  }
 `;
 
 const Title = styled.h2`
   font-size: 1.5rem;
   font-weight: ${WEIGHTS.medium};
+`;
+
+const SortWrapper = styled.div`
+  display: none;
+
+  @media ${QUERIES.tabletAndUp} {
+    display: revert;
+  }
+`;
+
+const MobileBreadCrumbs = styled.div`
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 export default ShoeIndex;
